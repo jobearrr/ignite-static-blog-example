415e34506e302c5d4e58f6e53ffde0d1a651e2d9
Jobert Sá <bearinmind@jobear.dev>
2024-05-26 11:31:28 +0200

    Added comments

--- Sources/Blog.swift
+++ Sources/Blog.swift
@@ -11,15 +11,22 @@ import Ignite
 @main
 struct Blog: Site {
     
+    // The name of your site. Required.
     var name = "My personal blog"
-    var url = URL("jobearrr.github.io")
     
+    // The base URL for your site (we're going to use GitHub Pages)
+    var url = URL("https://jobear.dev")
+    
+    // Assign the previously create theme to your website
     var theme = BlogTheme()
+    // Assign the previously created HomePage as homepage to your website
     var homePage = HomePage(title: "Home")
     
+    // This is the new application's entry point
     static func main() async {
         let blog = Blog()
         do {
+            // Will perform the publishing flow and generate the website content
             try await blog.publish()
         } catch {
             print(error.localizedDescription)
--- Sources/HomePage.swift
+++ Sources/HomePage.swift
@@ -9,15 +9,17 @@ import Foundation
 import Ignite
 
 struct HomePage: StaticPage {
+    // The title for this page.
     var title: String
     
     init(title: String) {
         self.title = title
     }
     
+    // Provides the content of this page using a block element builder.
     func body(context: PublishingContext) async -> [BlockElement] {
         Text {
             "Hello, world!"
-        }.font(.title1)
+        }.font(.title1) // translates into h1 HTML tag
     }
 }
