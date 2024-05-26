8142ef3da773971fbc1289076d08828e7c95ed73
Jobert Sá <bearinmind@jobear.dev>
2024-05-26 09:45:03 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Blog",
+    platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/twostraws/Ignite.git", revision: "0.2.1")
+    ],
+    targets: [
+        .executableTarget(
+            name: "Blog",
+            dependencies: ["Ignite"]),
+    ]
+)
